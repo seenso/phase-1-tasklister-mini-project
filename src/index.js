808a8fd7b5bdfb1c850a8ec10c53766360097ae0
seenso <>
2021-11-08 09:21:43 -0900

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+    
+    //create child node
+    let taskInput = document.getElementById("new-task-description").value;
+    let taskNode = document.createTextNode(taskInput);
+
+    //append child node to list
+    li.appendChild(taskNode);
+    ul.appendChild(li);
+
+    event.preventDefault();
+  });
+
+
 });
